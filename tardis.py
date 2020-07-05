db1002f0f2288ccfec790141aae8d9a0df4933a2
Richard Hopkins <hopkira@googlemail.com>
2020-07-06 00:15:20 +0100

    Correcting syntax errors

--- tardis.py
+++ tardis.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import json
 from requests import post
 
 sys.path.append('..')  # persistent import directory for K9 secrets
@@ -19,23 +20,20 @@ tardis_roof  = "light.33648804cc50e3ef7048"
 #off_url = "http://octopi.local:8123/api/services/light/turn_off"
 
 def device(domain,service,entity_id):
+    global URL
     URL = "http://" + URL + "/api/services/" + domain + "/" + service
     payload = { "entity_id" : entity_id }
     json_obj = json.dumps(payload)
-    response = post(URL, headers=headers, json=payload )
+    response = post(URL, headers=headers, json=json_obj )
     return response
 
 def light(light,status):
     if status :
-        {
-            response = device("light","turn_on",light)
-            return response
-        }
-    else
-        {
-            response = device("light","turn_off",light)
-            return response        
-        }
+        response = device("light","turn_on",light)
+        return response
+    else :
+        response = device("light","turn_off",light)
+        return response
 
 while True:
     try:
@@ -49,7 +47,7 @@ while True:
         time.sleep(1.0)
         print(response)
     except KeyboardInterrupt:
-        response = post(off_url, headers=headers, json=payload )
+        response = light(tardis_roof,0)
         print("Clean exit, light should go off")
         sys.exit(0)
 
