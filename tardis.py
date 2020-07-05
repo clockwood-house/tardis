a3831284ef4b3e0100efa62f1031f6b1989af2fa
Richard Hopkins <hopkira@googlemail.com>
2020-07-05 23:19:57 +0100

    Turn light on and off

--- tardis.py
+++ tardis.py
@@ -1,17 +1,29 @@
 import sys
-from requests import get
+import time
+from requests import post
 
 sys.path.append('..')  # persistent import directory for K9 secrets
 
 from k9secrets import token
 
+token = "Bearer " +  token
 
-url = "http://octopi.local:8123/api/services"
+on_url = "http://octopi.local:8123/light/turn_on"
+off_url = "http://octopi.local:8123/light/turn_off"
+payload = { "entity_id" : "light.33648804cc50e3ef7048"}
 headers = {
     "Authorization": token,
     "content-type": "application/json",
 }
 
-response = get(url, headers=headers)
-print(response.text)
+while True:
+    try:
+        response = get(on_url, headers=headers, json=payload )
+        time.sleep(1.0)
+        response = get(off_url, headers=headers, json=payload )
+        time.sleep(1.0)
+    except KeyboardInterrupt:
+        response = get(off_url, headers=headers, json=payload )
+        print("Clean exit, light should go off")
+
 
