ad1b3fbfad3782888127c8e5f0e997251af14bd2
Richard Hopkins <hopkira@googlemail.com>
2020-07-06 00:28:38 +0100

    Landing and taking off

--- tardis.py
+++ tardis.py
@@ -15,6 +15,7 @@ headers = {
 
 server = "octopi.local:8123"
 tardis_roof  = "light.33648804cc50e3ef7048"
+tardis_int = "light.33648804cc50e3ef73b5"
 
 #on_url = "http://octopi.local:8123/api/services/light/turn_on"
 #off_url = "http://octopi.local:8123/api/services/light/turn_off"
@@ -35,20 +36,23 @@ def light(light,status):
         response = device("light","turn_off",light)
         return response
 
-while True:
-    try:
-        #response = post(on_url, headers=headers, json=payload )
-        response = light(tardis_roof,1)
-        time.sleep(1.0)
-        print(response)
-        #response = post(off_url, headers=headers, json=payload )
-        #response = device(light,"turn_off",tardis_roof)
-        response = light(tardis_roof,0)
-        time.sleep(1.0)
-        print(response)
-    except KeyboardInterrupt:
-        response = light(tardis_roof,0)
-        print("Clean exit, light should go off")
-        sys.exit(0)
+for x in range(5):
+    response = light(tardis_roof,1)
+    time.sleep(1.0)
+    print(response)
+    response = light(tardis_roof,0)
+    time.sleep(1.0)
+    print(response)
+response = light(tardis_int,1)
+time.sleep(10.0)
+for x in range(5):
+    response = light(tardis_roof,1)
+    time.sleep(1.0)
+    print(response)
+    response = light(tardis_roof,0)
+    time.sleep(1.0)
+    print(response)
+response = light(tardis_int,0)
+
 
 
